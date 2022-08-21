4746269355cc56e4e6e8dd5b1d55c8156a3b1aaf
Mmd-Jalali <108706095+Mmd-Jalali@users.noreply.github.com>
2022-08-21 15:07:02 +0430

    Add files via upload

--- src/dashboardComponent/CardWithIcon.js
+++ src/dashboardComponent/CardWithIcon.js
@@ -1,67 +1,68 @@
 import * as React from 'react';
 import { FC, createElement } from 'react';
-import { Card, Box, Typography, Divider } from '@mui/material';
+import { Card, Box, Typography, Divider } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { ReactNode } from 'react';
 
 import cartouche from './cartouche.png';
 import cartoucheDark from './cartoucheDark.png';
 
-
 const CardWithIcon = (props) => {
     const { icon, title, subtitle, to, children } = props;
 
+const useStyles = makeStyles(theme => ({
+    card: {
+        minHeight: 52,
+        display: 'flex',
+        overflow: 'hidden',
+        float: 'right' ,
+        flexDirection: 'column',
+        flex: '1',
+        '& a': {
+            textDecoration: 'none',
+            color: 'inherit',
+        },
+    },
+    main: (props) => ({
+        overflow: 'hidden',
+        float: 'right' ,
+        backgroundSize:'contain',
+        padding: '16px',
+        minWidth:'270px',
+        background: `url(${
+            theme.palette.type === 'dark' ? cartoucheDark : cartouche
+        }) no-repeat`,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        '& .icon': {
+            color: theme.palette.type === 'dark' ? 'inherit' : '#dc2440',
+        },
+    }),
+    title: {},
+}));
+
+    const classes = useStyles(props);
     return (
-        // @ts-ignore
-        <Card
-            sx={{
-                minHeight: 52,
-                display: 'flex',
-                overflow: 'hidden',
-                float: 'right' ,
-                flexDirection: 'column',
-                flex: '1',
-                '& a': {
-                    textDecoration: 'none',
-                    color: 'inherit',
-                },
-            }}
-        >
+        <Card className={classes.card}>
             <Link to={to}>
-                <Box
-                    sx={{
-                        overflow: 'hidden',
-                        float: 'right' ,
-                        backgroundSize:'contain',
-                        padding: '16px',
-                        minWidth:'284px',
-                        background: theme =>
-                            `url(${
-                                theme.palette.mode === 'dark'
-                                    ? cartoucheDark
-                                    : cartouche
-                            }) no-repeat`,
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        '& .icon': {
-                            color: theme =>
-                                theme.palette.mode === 'dark'
-                                    ? 'inherit'
-                                    : '#dc2440',
-                        },
-                    }}
-                >
+                <div className={classes.main}>
                     <Box width="3em" className="icon">
                         {createElement(icon, { fontSize: 'large' })}
                     </Box>
                     <Box textAlign="right">
-                        <Typography color="textSecondary">{title}</Typography>
+                        <Typography
+                            className={classes.title}
+                            color="textSecondary"
+                        >
+                            {title}
+                        </Typography>
                         <Typography variant="h5" component="h2">
                             {subtitle || ' '}
                         </Typography>
                     </Box>
-                </Box>
+                </div>
             </Link>
             {children && <Divider />}
             {children}
