316aac622f9b4274f264fa175ae2b3bc15b49b95
Mmd-Jalali <108706095+Mmd-Jalali@users.noreply.github.com>
2022-10-20 11:34:07 +0330

    dashboard imp

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,14 +1,23 @@
 import * as React  from "react";
-//import { useMemo, CSSProperties } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { Title ,useGetList  } from 'react-admin';
 import WorkOrderCount from "./dashboardComponent/WorkOrderCount";
+import DepartmentBarCharts from "./dashboardComponent/DepartmentBarCharts";
+import AssetRequestBarChart from "./dashboardComponent/AssetRequestBarCharts";
+import WorkOrderSimpleList from "./dashboardComponent/OrderListSimple";
+import RequsetListSimple from "./dashboardComponent/RequsetListSimple";
+import ListTestDash from "./dashboardComponent/ListTestDash";
+
 
 export default () => (
     <Card>
         <Title title="داشبورد نرم‌افزار نگهداری و تعمیرات آویژه" />
-        <CardContent>صفحه اول نرم افزار شامل نمودارها و جداول مهم...</CardContent>
         <WorkOrderCount />
+        <DepartmentBarCharts/>
+        <AssetRequestBarChart/>
+        <WorkOrderSimpleList/>
+        <RequsetListSimple/>
+        <ListTestDash/>
     </Card>
 )
